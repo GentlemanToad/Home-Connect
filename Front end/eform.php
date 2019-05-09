e3710394d96d44da33ebe13f2a6ab09fd135ad3a
NoemiRMIT <NoemiRMIT@users.noreply.github.com>
2019-05-10 01:43:38 +1000

    Update eform.php
    Update login function to check for additioan error and return the errors. Also updated logout function

--- Front end/eform.php
+++ Front end/eform.php
@@ -21,15 +21,17 @@
 	// Function to check members username and password against database and set login session if correct otherwise return error
 	function loginMember($email,$password)
 	{
-		$pass = trim($password); //remove white space and predefined characters from entered password
+		$pass = sha1(trim($password)); //remove white space and predefined characters from entered password
 		include 'connect.php';  //connect to database
 		$stcount=$conn->prepare("SELECT User_ID FROM Users WHERE email=:email"); //sql query to return memberid based on email address
 		$stcount->bindPARAM(":email",$email,PDO::PARAM_STR); //binding email to php variable
 		$stcount->execute(); //run the sql query
 		$rcount=$stcount->rowCount(); //store number of rows returned based on query run above
 		$row = $stcount->fetch(PDO::FETCH_OBJ); //next row is returned and stored here
+		
 		if($rcount>0) //checks to see if an entry has been returned
 		{
+			
 			$userID = $row->User_ID; //assign userid returned to this variable
 			//check if session userid is set and contains the user_ID value
 			if(isset($_SESSION["id_loggedIn"]) && $_SESSION["id_loggedIn"]==$userID)
@@ -37,17 +39,22 @@
 				$conn = null; //disconnect database
 				return "Logged in already"; //return message member already logged in
 			}
+			
 			//Check for password
 			$stpass=$conn->prepare("SELECT Password, FailedAttempts FROM Users WHERE User_ID=:userid"); //sql query to return password based on userid
 			$stpass->bindPARAM(":userid",$userID,PDO::PARAM_INT); //binding userid to php variable
 			$stpass->execute(); //run the sql query
 			$stmembercount=$stpass->rowCount(); //store number of rows returned based on query run above
+			
 			$strow = $stpass->fetch(PDO::FETCH_ASSOC); //next row is returned and stored here
 			$attemptnum= $strow["FailedAttempts"];
-			if(($stusercount>0) AND ($attemptnum<3)) //checks to see if an entry has been returned
+			
+			if(($stmembercount>0) AND ($attemptnum<3)) //checks to see if an entry has been returned
 			{
-				if(password_verify($pass,$strow['Password'])) //verifies stored hashed password and entered password are the same
+				
+				if($pass==$strow['Password']) //verifies stored hashed password and entered password are the same
 				{
+					
 					$stattempt=$conn->prepare("UPDATE Users SET FailedAttempts = 0 WHERE User_ID=:userid");
 					$stattempt->bindPARAM(":userid",$userID,PDO::PARAM_INT);
 					$stattempt->execute(); //run the sql query
@@ -64,27 +71,31 @@
 					if($attemptnum<2)
 					{
 						$dbcount = 2 - $attemptnum;
-						$_SESSION["id_loggedIn"]=null; //id_loggedIn session is assigned a null as user's password entered did not match
+						unset($_SESSION["id_loggedIn"]); //id_loggedIn session is assigned a null as user's password entered did not match
 						$_SESSION["numberattempt"]=$dbcount;
-						exit;
+						$conn = null;
+						return $dbcount;
 					}
 					else
 					{
-						$_SESSION["id_loggedIn"]=null; //id_loggedIn session is assigned a null as user's password entered did not match
+						unset($_SESSION["id_loggedIn"]); //id_loggedIn session is assigned a null as user's password entered did not match
 						$_SESSION['attempttomany']=1; //set session to show member exceeded number of login attempts
-						exit;
+						$conn = null;
+						return 3;
 					}
 				}
 			}
 			else
 			{
-				$_SESSION["id_loggedIn"]=null; //id_loggedIn session is assigned a null as user's password entered did not match
+				
+				unset($_SESSION["id_loggedIn"]); //id_loggedIn session is assigned a null as user's password entered did not match
 				$_SESSION['attempttomany']=1; //set session to show member exceeded number of login attempts
-				exit;
+				return 3;
 			}
 		}
+		
 		$conn=null; //disconnect database
-		return null;
+		return "Not Exist";
 	}
 	
 	// Function to log member out
@@ -92,7 +103,7 @@
 	{
 		unset($_SESSION["id_loggedIn"]);
 		$_SESSION['test']="Log out";
-		header("Location: index.php"); //go to home page
+		header("Location:  http://localhost:81/Team-A/Frontend/index.php"); //go to home page
 		exit;
 	}
 	
