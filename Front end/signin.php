52d2cba32f2336e4dfb2fdcc1d2e0abec5e9b097
NoemiRMIT <NoemiRMIT@users.noreply.github.com>
2019-05-10 01:48:39 +1000

    Update signin.php
    Updated to include error message when user tries to login with incorrect details and lets them know of number attempts. Have replaced the form line to make it more secure, added a few name fields to assist with validation of database

--- Front end/signin.php
+++ Front end/signin.php
@@ -1,5 +1,45 @@
-
-<!doctype html>
+<?php
+	
+	if(!isset($_SESSION)){session_start();}
+	require_once('eform.php');
+	
+	
+	$in=0;
+	$errors = "";
+	$message = "";
+	if (($_SERVER['REQUEST_METHOD'] == 'POST') && (isset($_POST['signin']))) //check to see if Login form submit button has been clicked
+	{
+	
+		//include('eform.php'); //call the php validation form
+		$in = loginMember($_POST['email'],$_POST['password']); //send user login details to validation form and return result
+		
+		if ($in=="Logged in already")
+		{
+			$error = "You are already logged in";
+		}
+		elseif ($in=="Not Exist")
+		{
+			$error = "User account does not exist, please try again";
+		}
+		elseif ($in=='3')
+		{
+			$error = "Incorrect username or password. User blocked, try again after 24 hours";
+			
+		}
+		elseif ($in<3)
+		{
+			$error = "Incorrect username or password, you have ".$in." attempts left";
+			
+		}
+		else
+		{
+			echo $in;
+			exit;
+			//header("Location: http://localhost:81/Team-A/Frontend/index.php");
+		}
+		
+	}
+?>
 <html lang="en">
   <head>
     <meta charset="utf-8">
@@ -34,31 +74,36 @@
     <!-- Custom styles for this template -->
     <link href="signin-style.css" rel="stylesheet">
   </head>
-  <body>
-    <form class="form-signin">
-  <div class="text-center mb-4">
-    <img class="mb-4" src="../media/HC-TransparentLogo.png" alt="" width="120" height="72">
-    <h1 class="h3 mb-3 font-weight-normal">Home Connect</h1>
-    <p>To continue, please sign in with your email and password.</p>
-  </div>
+	<body>
+		<form class="form-signin" method="POST" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>"  > <!--This assigns id to login form and provides the way system is to perform an action once submit button has been clicked by user-->
+		  <div class="text-center mb-4">
+			<img class="mb-4" src="../media/HC-TransparentLogo.png" alt="" width="120" height="72">
+			<h1 class="h3 mb-3 font-weight-normal">Home Connect</h1>
+			<?php if (($_SERVER['REQUEST_METHOD'] == "POST") && (isset($error)))
+				{?>
+					<span class="errormsg"><?php echo $error ?></span>
+				<?php } else{?>
+				<p>To continue, please sign in with your email and password.</p>
+				<?php }?>
+		  </div>
 
-  <div class="form-label-group">
-    <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
-    <label for="inputEmail">Email address</label>
-  </div>
+		  <div class="form-label-group">
+			<input type="email" name="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+			<label for="inputEmail">Email address</label>
+		  </div>
 
-  <div class="form-label-group">
-    <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
-    <label for="inputPassword">Password</label>
-  </div>
+		  <div class="form-label-group">
+			<input type="password" name="password" id="inputPassword" class="form-control" placeholder="Password" required>
+			<label for="inputPassword">Password</label>
+		  </div>
 
-  <div class="checkbox mb-3">
-    <label>
-      <input type="checkbox" value="remember-me"> Remember me
-    </label>
-  </div>
-  <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-  <p class="mt-5 mb-3 text-muted text-center"> &copy; Home Connect by Team A - Building IT Systems 2019</p>
-</form>
-</body>
+		  <div class="checkbox mb-3">
+			<label>
+			  <input type="checkbox" value="remember-me"> Remember me
+			</label>
+		  </div>
+		  <button class="btn btn-lg btn-primary btn-block" value="signin" name="signin" type="submit">Sign in</button>
+		  <p class="mt-5 mb-3 text-muted text-center"> &copy; Home Connect by Team A - Building IT Systems 2019</p>
+		</form>
+	</body>
 </html>
